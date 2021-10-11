ee9ff72f121b928865657bdfa3fc5efcb111d7f9
juani11 <juanitagliero@gmail.com>
2021-10-11 11:43:02 -0300

    cart utils para agrupar items (con quantity) en cart reducer

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -11,7 +11,7 @@ const CollectionPreview = ({ title, items }) => {
             <div className="preview">
                 {items
                     .filter((item, id) => id < 4)
-                    .map(({ id, ...rest }) => <CollectionItem key={id} {...rest} />)
+                    .map(item => <CollectionItem key={item.id} item={item} />)
                 }
             </div>
         </div>
